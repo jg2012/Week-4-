373093806492e2576a512797fe6f47809efd4f9a
jg764609 <jg764609@southeast.edu>
2022-07-26 21:00:06 -0500

    Open Class updates.

---
+++ src/Openclass.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Openclass {
+
+	static void createList(int endNum) {
+	List<Integer> number = new ArrayList<Integer>();
+	for(int num: number) { 
+		num; 
+	}
+	
+	public static void main(String[] args) {
+
+		
+		
+		Scanner in = new Scanner(System.in);
+		int endNum = in.nextInt();
+		
+		createList(endNum);
+	}
+		
+	
+
+}
